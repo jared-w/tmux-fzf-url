0b17234ee383260acd8ba277e4099700683a655d
jared-w <jweakly@pdx.edu>
2018-06-27 17:22:40 -0700

    Add localhost urls that don't contain a .

--- fzf-url.sh
+++ fzf-url.sh
@@ -21,6 +21,7 @@ urls=($(echo "$content" |
 wwws=($(echo "$content" |
   rg -o -N -e '(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)' \
            -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]{1,5})?(/\S+)*' --replace 'https://$0'))
+locals=($(echo "$content" | rg -o -N 'https?://.+:.+'))
 
 # Ends up being n log n
 containsElement () {
@@ -38,7 +39,7 @@ merge() {
     done
 }
 
-merge "${urls[@]}" "${wwws[@]}"  |
+merge "${urls[@]}" "${wwws[@]}" "${locals[@]}" |
     sort -u -f                   |
     nl -w3 -s '  '               |
     fzf_cmd                      |
